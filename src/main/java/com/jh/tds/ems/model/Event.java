a8e2b0a795c3e6d7fb5264b4727756c4b99b1287
java-hangout <contacts.veeresh@gmail.com>
2025-02-06 10:42:01 +0530

    updated fields

--- src/main/java/com/jh/tds/ems/model/Event.java
+++ src/main/java/com/jh/tds/ems/model/Event.java
@@ -7,7 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
@@ -20,12 +19,7 @@ public class Event {
     private String title;
     private String description;
     private String type; //ACHIEVEMENT, TOWNHALL, CULTURAL_ACTIVITY
-//    private LocalDateTime date;
     private boolean eventActiveFlag;
-
-//    private String departmentId;           // Reference to the department the task belongs to (Many-to-One)
-//    private String departmentName;           // Reference to the department the task belongs to (Many-to-One)
-//    private String businessUnitId;
     private String businessUnitName;
     @CreatedDate
     private Date createdDate;  // Stores the date when the record was created
